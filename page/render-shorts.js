10b9b1ec5de8c903c4105db057f1e10e41a570f8
Robin JALLADE <114308804+shotoly@users.noreply.github.com>
2026-06-04 22:33:35 +0200

    Add files via upload

--- page/render-shorts.js
+++ page/render-shorts.js
@@ -112,7 +112,7 @@ globalThis.playShort = function(videoId, wrapper, videoType = 'youtube') {
                 controls 
                 autoplay 
                 playsinline
-                style="position: absolute; top:0; left:0; width:100%; height:100%; border:none; object-fit: cover; background: #000;">
+                style="position: absolute; top:0; left:0; width:100%; height:100%; border:none; object-fit: contain; background: #000;">
             </video>`;
     } else {
         iframeHtml = `
