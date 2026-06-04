07b53333f3004d0ef2d29288d3786817743de63a
Robin JALLADE <114308804+shotoly@users.noreply.github.com>
2026-06-04 22:57:23 +0200

    Add files via upload

--- page/render-shorts.js
+++ page/render-shorts.js
@@ -106,6 +106,7 @@ globalThis.playShort = function(videoId, wrapper, videoType = 'youtube') {
         iframeHtml = `
             <video 
                 src="${videoId}" 
+                controls
                 autoplay 
                 playsinline
                 style="position: absolute; top:0; left:0; width:100%; height:100%; border:none; object-fit: contain; background: #000;">
